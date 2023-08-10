adc99f90dd5e69c05972fbc2fbea0145e00ae5f0
Iamneo <techadmin@iamneo.ai>
2023-08-10 08:29:50 +0000

    Backup Commit

--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -2,4 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.jsx";
 
-ReactDOM.render(<h1>Hello</h1>, document.getElementById("root"));
+ReactDOM.render(<App></App>, document.getElementById("root"));
